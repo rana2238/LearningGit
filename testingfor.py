e935e4030daf7cc2005d538edda43129578b340b
Rahul Rana <rarana@bebotechnologies.com>
2023-10-04 13:10:46 +0530

    change for baluu bhaiya

--- testingfor.py
+++ testingfor.py
@@ -2,4 +2,4 @@
 print("this is a testing tool")
 print("tell ne the code if the followung")
 print("this is agian tested for the merge purposr")
-print("tell me the changes only being made for the tetting")
+print("tell me the changes only being made for thg")
